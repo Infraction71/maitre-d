9a59817acdac49296f2281691160bd05de8283b9
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2020-02-17 12:32:22 -0600

    Completed project
    Forgot to commit partail changes on Wednesday. Whoops....

---
+++ README.md
@@ -0,0 +1,5 @@
+#Sources Used 
+Please note this does not include any soruces from the previous web apps project I did last week.
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/flex-wrap
+https://www.w3schools.com/howto/howto_css_hide_arrow_number.asp
---
+++ main.js
@@ -0,0 +1,17 @@
+function getGrndTotal(){
+
+
+    let subTotal = parseFloat(document.getElementById("subTotal").value);
+    console.log("Testing. Is this actually working?");
+    let tip = parseFloat(document.getElementById("tip").value);
+    tip /=100; //taking tip in as a percentage, NOT a decimal
+    const TAX = .055; //Setting variable in case tax chnages. This is Missouri so that's a joke obviously. 
+    if (subTotal == "" || tip == ""){
+        alert("Please make sure both fields have appropriate values"); //Error checking
+    }
+    //Printing stuff
+    document.getElementById("tipOut").innerHTML = "Tip: $" + subTotal*tip;
+    document.getElementById("taxOut").innerHTML = "Tax: $" + subTotal*TAX;
+    document.getElementById("grandTotal").innerHTML = "Grand Total: $" + (subTotal + (subTotal*tip) + (subTotal*TAX));
+    
+}
